81ed0b040e0efd98af816f2d2f0fc19a271818c4
manuel <manuelcarranza96@hotmail.com>
2018-12-19 16:45:01 +0100

    Comisiones.js modified

---
+++ comisiones.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+
+var comisionSchema = new mongoose.Schema({
+    investigadorID: String,
+    destino: String,
+    fechaInicio: Date,
+    fechaFin: Date,
+    sustitutoID: String,
+    razon: String,
+    coste: Number,
+    proyectoID: String,
+    estado: String
+});
+
+comisionSchema.methods.cleanup = function() {
+    return {investigadorID = this.investigadorID,
+        destino = this.destino,
+        fechaInicio: this.fechaInicio,
+        fechaFin: this.fechaFin,
+        sustitutoID: this.sustitutoID,
+        razon: this.razon,
+        coste: this.coste,
+        proyectoID: this.proyectoID,
+        estado: this.estado};
+}
+
+var Comision = mongoose.model('Comision', comisionSchema);
+
+module.exports = Comision;
