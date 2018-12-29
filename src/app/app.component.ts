4d3bff8f4c5483891b8bf24a70d9d8b16a4bdc09
José Gallardo <josegallardog95@gmail.com>
2018-12-29 23:24:22 +0100

    Change auth0 to public.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent implements OnInit{
 
-  constructor(private auth: AuthService) {
+  constructor(public auth: AuthService) {
     auth.handleAuthentication();
   }
 
