a7087cdb76802676694e73dcf8a3269b376e0395
Miguel Esteban Gómez <miestgo@gmail.com>
2019-01-07 13:36:35 +0100

    Empezando pacto con grupo 1.

--- client-demo/mockServer/interactions.js
+++ client-demo/mockServer/interactions.js
@@ -1,16 +1,52 @@
-const ONE_CONTACT_BODY = [{ name: 'Foo', phone: 777 }]
+const RESEARCHERS_BODY = [
+    {name:"Manuel",
+    surname:"Carranza",
+    dni:"00000001A",
+    certification:"Associate Professor",
+    studyCentre:"Universidad de Sevilla",
+    scientificTechnicalArea:[],
+    department:"LSI",
+    responsible:false,
+    active:true,
+    entryDate:"2019-01-07T12:15:24.964Z",
+    publications:[]},
+
+    {name:"Miguel",
+    surname:"Esteban",
+    dni:"00000003C",
+    certification:"Associate Professor",
+    studyCentre:"Escuela Tecnica Superior de Ingenieria Informatica",
+    scientificTechnicalArea:["Deep Learning","Machine Learning"],
+    department:"LSI",
+    responsible:false,
+    active:true,
+    entryDate:"2019-01-07T12:26:17.916Z",
+    publications:[]},
+
+    {name:"José",
+    surname:"Gallardo Gallardo",
+    dni:"00000002B",
+    certification:"Associate Professor",
+    studyCentre:"Escuela Tecnica Superior de Ingenieria Informatica",
+    scientificTechnicalArea:["Cloud","SLA"],
+    department:"LSI",
+    responsible:false,
+    active:true,
+    entryDate:"2019-01-07T12:28:26.729Z",
+    publications:[]}
+]
 
 module.exports = {
-    getContactList: {
-        state: 'it has one contact',
-        uponReceiving: 'a request to retrieve contacts list',
+    getResearcherList: {
+        state: 'it has 3 researchers',
+        uponReceiving: 'a request to retrieve the list of researchers',
         withRequest: {
             method: 'GET',
-            path: '/api/v1/contacts'
+            path: '/api/v1/researchers'
         },
         willRespondWith: {
             status: 200,
-            body: ONE_CONTACT_BODY
+            body: RESEARCHERS_BODY
         }
     }
 }
