018db83ea213c093a2729085c37e19e87c2506cb
Miguel Esteban Gómez <miestgo@gmail.com>
2019-01-07 20:58:19 +0100

    pacto con el grupo 1 creado.

--- client-demo/researchersResource.js
+++ client-demo/researchersResource.js
@@ -1,7 +1,7 @@
 var urljoin = require('url-join');
 var request = require('request-promise-native').defaults({json: true});
 var CommandFactory = require('hystrixjs').commandFactory;
-var researchersServer = (process.env.RESEARCHERS_URL || 'http://localhost:3000/api/v1/researchers'); // TODO: check url is right.
+var researchersServer = (process.env.RESEARCHERS_URL || 'http://localhost:3000/api/v1/'); // TODO: check url is right.
 var researchersKey = (process.env.RESEARCHERS_APIKEY || '9806869c-22f0-4834-8025-77607ad275f6-52ad-41ae-887c-34c029d48242'); // Using heroku apikey as given by that group.
 
 function researchersResource(url) {
