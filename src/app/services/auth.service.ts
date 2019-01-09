fc3b8ee169c0874a3885855d80b1dae2c401046a
manuel <manuelcarranza96@hotmail.com>
2019-01-09 01:30:53 +0100

    Revert "Merge pull request #43 from Sojer23/apiKey_sessionStorage"
    This reverts commit d5f446761378fb227509903287dc0a66c1680de5, reversing
    changes made to 34c67a474cff05336a3edff6bcf9dd9c656ad114.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -122,7 +122,7 @@ private handleError<T> (operation = 'operation', result?: T) {
     }
 
 
-    public getApiKey(user:String): Observable<any> {
+    private getApiKey(user:String): Observable<any> {
       let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
       const url = `${this.comisionesUrl}/comisiones/login`;
       let appUser = {user: `${user}`, password: "app_pass"}
